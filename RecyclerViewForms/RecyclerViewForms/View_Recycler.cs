2049cc1a1d409e2bb97e6820cc4755e48e38f087
Luis David De la cruz <delacruz_luis@outlook.es>
2018-02-17 20:10:07 -0600

    Beautiful CardView in Xamarin.Forms / PCL or .Net Standard

---
+++ RecyclerViewForms/RecyclerViewForms/View_Recycler.cs
@@ -0,0 +1,56 @@
+﻿    using Cards;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+namespace RecyclerViewForms
+{
+    class View_Recycler: ContentPage
+    {
+        CardData cards = new CardData();
+        StackLayout cardstack = new StackLayout
+        {
+            Spacing = 15,
+            Padding = new Thickness(10),
+            VerticalOptions = LayoutOptions.StartAndExpand,
+        };
+
+        StackLayout shadowcardstack = new StackLayout
+        {
+            Spacing = 5,
+            Padding = new Thickness(5),
+            VerticalOptions = LayoutOptions.StartAndExpand,
+        };
+        public View_Recycler()
+        {
+            this.Title = "ReclerVier/CardView";
+            CargaLista();
+            
+        }
+        public void CargaLista()
+        {
+            foreach (var card in cards)
+            {
+                cardstack.Children.Add(new CardView(card));
+            }
+            this.BackgroundColor = Color.White;
+            
+            Content = new ScrollView()
+            {
+                Content = new StackLayout()
+                {
+                    
+                    Spacing=30,
+                    Padding=25,
+                    Children =
+                    {
+                            
+                            cardstack,
+                            shadowcardstack
+                    }
+                }
+            };
+            
+        }
+    }
+}
